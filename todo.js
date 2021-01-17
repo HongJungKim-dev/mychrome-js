6ae15456a7edef6c041dd87dca5586239d12c6e1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-17 23:30:35 +0900

    Save name and todo
    입력된 이름과 todo들을 저장합니다.

--- todo.js
+++ todo.js
@@ -4,15 +4,27 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "❌";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.di = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
 }
 function handleSubmit(event) {
     event.preventDefault();
@@ -22,8 +34,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
